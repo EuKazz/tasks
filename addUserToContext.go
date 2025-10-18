693a034d1a1ac103224da8326b251b872aba413c
Eugenia Kazz <eu.kazachenok@yandex.ru>
2025-10-18 17:00:45 +0300

    Update addUserToContext.go

--- addUserToContext.go
+++ addUserToContext.go
@@ -14,3 +14,43 @@
 	//
 	// Input: r = HTTP-запрос, userID = "admin456"
 	// Output: новый HTTP-запрос с admin456 в контексте
+package main
+
+import (
+	"context"
+	"net/http"
+  "io"
+  "bytes"
+)
+
+// Определяем тип ключа для контекста
+type contextKey string
+
+const UserIDKey contextKey = "userID"
+
+func addUserToContext(r *http.Request, userID string) *http.Request {
+  if userID==""{
+    return nil
+  }
+  if r == nil{
+    return nil
+  }
+  method:= r.Method
+  url:= r.URL.String()
+  bodyBytes, err:= io.ReadAll(r.Body)
+  if err != nil {
+        return nil
+    }
+  defer r.Body.Close()
+  //после чтения запроса через ioданные надо снова записать для нового запроса
+  body:= bytes.NewReader(bodyBytes)
+  
+
+  ctxBack:= r.Context()
+  ctx:= context.WithValue(ctxBack, UserIDKey,userID )
+  req,err:= http.NewRequestWithContext(ctx, method, url, body)
+  if err!=nil{
+    return nil
+  }
+	return req
+}
