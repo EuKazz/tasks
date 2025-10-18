3c327b91601258a3534f1192cb4bae63e68e6e2b
Eugenia Kazz <eu.kazachenok@yandex.ru>
2025-10-18 11:30:40 +0300

    Update parsejwt.go

--- parsejwt.go
+++ parsejwt.go
@@ -14,3 +14,45 @@
 	//
 	// Input: tokenString = "", secret = "mysecretkey123"
 	// Output: "", error("token cannot be empty")
+package main
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+  "fmt"
+)
+
+//структура для распаковки всех полей из пейлоад токена
+type MyClaims struct{
+  Email string `json:"email"`
+  jwt.RegisteredClaims
+}
+
+func parseJWTToken(tokenString string, secret string) (string, error) {
+  if tokenString == ""{
+      return "", errors.New("token cannot be empty")
+    }
+  if len(secret)<8{
+    return "", errors.New("secret must be at least 8 characters long")
+  }
+  claims:= &MyClaims{}
+  //парсинг(в claims запишутся декодированные данные)
+  token, err:= jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token)(interface{}, error){
+  //проверка метода подписи
+    if _, ok:= token.Method.(*jwt.SigningMethodHMAC);!ok{
+    return nil, fmt.Errorf("unexpected signing method")
+  }
+    return []byte(secret), nil
+  })
+
+  if err!=nil{
+    return "",err
+  }
+
+  if !token.Valid{
+     return "", fmt.Errorf("invalid token")
+  }
+   
+  return claims.Email, nil
+
+}
