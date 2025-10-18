563d51d44712661e75a6dca00212dfc84554e9ea
Eugenia Kazz <eu.kazachenok@yandex.ru>
2025-10-18 13:51:51 +0300

    Update middleware_chain.go

--- middleware_chain.go
+++ middleware_chain.go
@@ -15,3 +15,18 @@
 // Input: Chain(corsMiddleware, rateLimitMiddleware, authMiddleware)
 // Output: Функция, которая применит middleware в порядке:
 // authMiddleware → rateLimitMiddleware → corsMiddleware
+package main
+
+import "net/http"
+
+
+type MiddleWare func(http.Handler) http.Handler
+
+func Chain(middlewares ...MiddleWare) MiddleWare {
+	return func(next http.Handler) http.Handler {
+		for i := len(middlewares) - 1; i >= 0; i-- {
+			next = middlewares[i](next)
+		}
+		return next
+	}
+}
