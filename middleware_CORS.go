05ddfb71ae17944ea96dfd941f0179a314346177
Eugenia Kazz <eu.kazachenok@yandex.ru>
2025-10-18 13:22:22 +0300

    Update middleware_CORS.go

--- middleware_CORS.go
+++ middleware_CORS.go
@@ -14,3 +14,39 @@
 	//
 	// Input: GET запрос с Origin: "https://example.com"
 	// Output: Обычный ответ с добавленными CORS заголовками
+package main
+
+import (
+	"net/http"
+)
+
+func CORS(next http.Handler) http.Handler {
+	
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	//получаем заголовок ориг из запроса (он присутствует при кросплатф запросе)
+    origin:= r.Header.Get("Origin")
+    //если его нет - значит запрос прямой или внутренний - вызываем след обработчик без корс-заголовков и прекращаем работу мидлвэр
+      if origin == ""{
+      next.ServeHTTP(w,r)
+      return
+    }
+      //получаем ссылку на заголовки ответа
+      header:= w.Header()
+      //разрешаем доступ этому источнику
+      header.Set("Access-Control-Allow-Origin", origin)
+      //разрешает браузеру отправлять куки, авторизационные заголовки
+      header.Set("Access-Control-Allow-Credentials", "true")
+		if r.Method == http.MethodOptions {
+          //если пришел префлайт запрос указываем разрешенные методы
+			header.Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, PATCH")
+		  //указываем разрешенные заголовки
+            header.Set("Access-Control-Allow-Headers", "authorization, content-type")
+		  //указываем макс время хранения ответа браузером(1 сутки)
+            header.Set("Access-Control-Max-Age", "86400")
+          //отправляем ОК для завершения обработки запроса и выходим из мидлвэр 
+            w.WriteHeader(http.StatusOK) 
+			return
+		}
+      next.ServeHTTP(w,r)
+	})
+}
