7f109b28aa704669ae3e58f6ca83dbc8bae9302e
Eugenia Kazz <eu.kazachenok@yandex.ru>
2025-10-18 16:32:00 +0300

    Update jwt_parse.go

--- jwt_parse.go
+++ jwt_parse.go
@@ -20,3 +20,49 @@
 	// Input: tokenString = "invalid.token.string",
 	// secretKey = []byte("secret")
 	// Output: JWTData{}, error
+package main
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+  
+)
+
+type JWTData struct {
+	Email string
+}
+
+type MyClaims struct {
+    Email string `json:"email"`
+    jwt.RegisteredClaims
+}
+
+func Parse(tokenString string, secretKey []byte) (*JWTData, error) {
+if tokenString==""{
+  return nil, errors.New("token cannot be empty")
+}
+   if len(secretKey) == 0 {
+        return nil, errors.New("secret key is empty")
+    }
+ claims:= &MyClaims{}
+token, err:= jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token)(interface{}, error){
+  //проверка метода подписи
+    if _, ok:= token.Method.(*jwt.SigningMethodHMAC);!ok{
+    return nil, fmt.Errorf("unexpected signing method")
+  }
+    return []byte(secretKey), nil
+  })
+
+  if err!=nil{
+    return nil,err
+  }
+
+  if !token.Valid{
+     return nil, fmt.Errorf("invalid token")
+  }
+ 
+ if claims.Email == ""{
+    return nil, errors.New("email not found in token claims")
+ }
+ return &JWTData{Email: claims.Email}, nil
+}
