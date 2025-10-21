4105638f8defc925c6f243ce518da0edf066690e
Eugenia Kazz <eu.kazachenok@yandex.ru>
2025-10-21 21:48:19 +0300

    Update test_db.go

--- test_db.go
+++ test_db.go
@@ -17,3 +17,46 @@
     //
     // Input: dbName = "", config = DatabaseConfig{}
     // Output: nil, nil, error "database name is required"
+package main
+
+import (
+    "gorm.io/gorm"
+    "gorm.io/driver/sqlite"
+  "errors"
+)
+
+type DatabaseConfig struct {
+    Driver string
+    Host   string
+    Port   int
+    Name   string
+}
+
+type User struct {
+    ID    int
+    Email string
+}
+
+func setupTestDatabase(dbName string, config DatabaseConfig) (*gorm.DB, func(), error) {
+if dbName==""{
+  return nil, nil, errors.New("database name is required")
+}
+  if config.Driver != "sqlite"{
+    return nil, nil, errors.New("driver is wrong")
+  }
+  db, err:= gorm.Open(sqlite.Open(dbName), &gorm.Config{})
+if err!=nil{
+  return nil, nil, err
+}  
+  //низкоуровневое соединений
+  sqlDB, err:= db.DB()
+  if err!=nil{
+    return nil, nil, err
+  }
+  //функция очистки соединения
+  cleanup:= func(){
+    sqlDB.Close()
+  }
+  db.AutoMigrate(&User{})
+    return db, cleanup, nil
+}
