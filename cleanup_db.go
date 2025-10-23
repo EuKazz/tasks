c0f0af87e05f941857ccd5aebd158574b72b217a
Eugenia Kazz <eu.kazachenok@yandex.ru>
2025-10-23 22:17:03 +0300

    Update cleanup_db.go

--- cleanup_db.go
+++ cleanup_db.go
@@ -16,3 +16,51 @@
 //
 // Input: removeData(db) после выполнения тестов
 // Output: База данных полностью очищена от тестовых записей
+package main
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID       uint   `gorm:"primaryKey"`
+	Email    string `gorm:"unique"`
+	Password string
+}
+
+
+
+func initData(db *gorm.DB, user User) error {
+  
+  result:=db.Create(&user)
+
+	return result.Error
+}
+
+func removeData(db *gorm.DB) error {
+	return db.Unscoped().
+      Delete(&User{}).
+      Error
+	
+}
+
+func initDb() *gorm.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func main() {
+  db := initDb()
+  user := User{Email: "test@example.com", Password: "hashed123"}
+  initData(db, user)
+  //
+  removeData(db)
+}
